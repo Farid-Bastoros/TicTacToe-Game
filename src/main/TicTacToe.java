731cd7193eb0b63c46a5ca8b737c19c694f26e81
Farid Bastoros <faridbastoros@Farids-MacBook-Pro.local>
2024-05-26 03:22:04 -0400

    Initial Commit

---
+++ src/main/TicTacToe.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe extends JFrame {
+
+    public TicTacToe() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
